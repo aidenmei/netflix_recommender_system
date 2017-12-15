8564d0cae8944e0b07968f6b329401b024913d41
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-12-16 00:25:14 +0800

    Update

--- run.sh
+++ run.sh
@@ -9,10 +9,19 @@ hdfs dfs -rm -r /multiplication
 hdfs dfs -rm -r /summation
 
 hdfs dfs -mkdir /input
-hdfs dfs -put ../dataset/* /input
+
+wget -c https://s3-us-west-2.amazonaws.com/wengaoye/MovieBigDataSet.tar
+tar -xvf MovieBigDataSet.tar
+
+hdfs dfs -put MovieBigDataSet/input/* /input
+
+cd map_reduce_job/
 
 hadoop com.sun.tools.javac.Main *.java
 
 jar cf recommender.jar *.class
 
 hadoop jar recommender.jar Driver /input /dataDividedByUser /coOccurrenceMatrix /normalize /averageRating /movieList /ratingMatrix /multiplication /summation
+
+mkdir ../result
+hdfs dfs -get /summation/part-r-00000 ../result/result.txt
